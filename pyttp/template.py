797e32af0cb299b9171515a1877f1808237039f9
daineX <puseidel@gmail.com>
2012-06-13 14:20:21 +0200

    Refactored template

--- pyttp/template.py
+++ pyttp/template.py
@@ -2,46 +2,83 @@
 import re
 
 
-class Template(object):
+TAB_INDENT = 4
+
+
+class Node(object):
+
 
     class ParseError(Exception):
         pass
 
-    TAB_INDENT = 4
+    class Error(Exception):
+        pass
+
+
+    def __init__(self, context, line):
+        self.context = context
+        self.line = line
+        self.children = []
+
+
+    def add_child(self, child):
+        self.children.append(child)
+
+
+    def render(self, indent):
+        return ''.join(x.render(indent) for x in self.children)
+
+    @property
+    def is_tag(self):
+        return False
+
+    def eval_code(self, context, markup):
+        value = eval(markup, globals(), context)
+        if callable(value):
+            value = value()
+        return value
+
+    def __repr__(self):
+        return "Node(%s, %s)" % (self.context, self.line)
+
+
+class TextNode(Node):
+
+    def render(self, indent):
+        return self.line
+
+
+    def add_child(self, child):
+        raise Node.Error("A TextNode may not have any children")
+
+
+
+class TagNode(Node):
 
     TAG_RE = r"%(?P<tag_name>\w[\w#\.]*)(\((?P<attrs>.+)\))?(?P<value_insert>=)?(?P<remainder>.+)?"
-    TAG_NAME_RE = r"(?P<name>\w+)"
     TAG_CLASS_RE = r"\.(?P<class>\w+)"
+    TAG_NAME_RE = r"(?P<name>\w+)"
+
     TAG_ID_RE = r"#(?P<id>\w+)"
-    ATTR_RE = r"(?P<key>\w+):\s*'(?P<value>.+?)',?", r"(?P<key>\w+):\s*\"(?P<value>.+?)\",?"
+    ATTR_RE = r"(?P<key>\w+):\s*'(?P<value>.+?)',?", r"(?P<key>\w+):\s*\"(?P<value>.+?)\",?"    
 
-    def is_comment(self, line):
-        return line.strip().startswith('//')
+    def __init__(self, context, line):
+        super(TagNode, self).__init__(context, line)
 
+        (self.tag_name, self.attrs,
+         self.is_value_insert, self.remainder) = self._parse_tag()
 
-    def is_tag(self, line):
-        return line.strip().startswith('%')
-
-
-    def is_value_insert(self, line):
-        return line.startswith('=')
+        self.attrs = self._parse_attrs(self.attrs)
+        self.tag_name, self.attrs = self._handle_shortcuts(self.tag_name, self.attrs)
 
 
-    def indentation_depth(self, line):
-        indent = 0
-        for char in line:
-            if char == ' ':
-                indent += 1
-            elif char == '\t':
-                indent += Template.TAB_INDENT
-            else:
-                break
-        return line.lstrip(), indent
+    @property
+    def is_tag(self):
+        return True
 
 
-    def parse_tag(self, stripped_line):
-
-        m = re.match(Template.TAG_RE, stripped_line)
+    def _parse_tag(self):
+        m = re.match(TagNode.TAG_RE, self.line)
         tag_name = m.group('tag_name')
         attrs = m.group('attrs')
         is_value_insert = bool(m.group('value_insert'))
@@ -53,15 +90,15 @@ class Template(object):
         return tag_name, attrs, is_value_insert, remainder
 
 
-    def parse_attrs(self, attrs):
+    def _parse_attrs(self, attrs):
         parsed_attrs = []
         while attrs:
-            for regex in Template.ATTR_RE:
+            for regex in TagNode.ATTR_RE:
                 m = re.match(regex, attrs)
                 if m:
                     break
             else:
-                raise Template.ParseError("Failed to parse attributes, remainder was %s" % attrs)
+                raise Node.ParseError("Failed to parse attributes, remainder was %s" % attrs)
             key, value = m.group('key'), m.group('value')
             parsed_attrs.append((key, value))
             attrs = attrs[m.end():].lstrip()
@@ -69,28 +106,21 @@ class Template(object):
         return parsed_attrs
 
 
-    def handle_div(self, line):
-        if line.startswith(".") or line.startswith("#"):
-            return "%div" + line
-        else:
-            return line
-
-
-    def handle_shortcuts(self, tag_string, attrs):
+    def _handle_shortcuts(self, tag_string, attrs):
         original_string = tag_string
-        m = re.match(Template.TAG_NAME_RE, tag_string)
+        m = re.match(TagNode.TAG_NAME_RE, tag_string)
         name = m.group("name")
         tag_string = tag_string[m.end():]
 
         classes = []
         tag_id = None
         while tag_string:
-            m = re.match(Template.TAG_CLASS_RE, tag_string)
+            m = re.match(TagNode.TAG_CLASS_RE, tag_string)
             if m:
                 classes.append(m.group("class"))
                 tag_string = tag_string[m.end():]
                 continue
-            m = re.match(Template.TAG_ID_RE, tag_string)
+            m = re.match(TagNode.TAG_ID_RE, tag_string)
             if m:
                 tag_id = m.group("id")
                 tag_string = tag_string[m.end():]
@@ -109,15 +139,25 @@ class Template(object):
         return name, attrs
 
 
-    def eval_code(self, context, markup):
-        value = eval(markup, globals(), context)
-        if callable(value):
-            value = value()
-        return value
+    def render(self, indent):
+
+        start = self._render_tag_start(self.context, indent, self.tag_name, self.attrs, self.remainder, self.is_value_insert)
+
+        childs = ''
+        close = ''
+        if self.remainder and self.children:
+            childs = ' '
+        if self.children:
+            childs += ' '.join(child.render(indent + 1) for child in self.children)
+        new_line = any(x.is_tag for x in self.children)
+        if self.children:
+            close = self._render_closing_tag(indent, self.tag_name, new_line=new_line)
+
+        return start + childs + close
 
 
-    def render_tag_start(self, context, indent, tag_name, attrs, remainder, eval_remainder=False):
-        html = ['\n', ' ' * indent]
+    def _render_tag_start(self, context, indent, tag_name, attrs, remainder, eval_remainder=False):
+        html = ['\n', ' ' * indent * TAB_INDENT]
         html.append('<%s' % tag_name)
         if attrs:
             html.append(' ')
@@ -129,106 +169,110 @@ class Template(object):
             html.append(' '.join(evaluated_attrs))
         if eval_remainder and remainder:
             remainder = self.eval_code(context, remainder.lstrip())
+        if self.children:
+            html.append(self._render_tag_end(one_line=False))
+        else:
+            html.append(self._render_tag_end(one_line=True))
         if remainder:
-            html.append(self.render_tag_end(one_line=False))
             html.append(remainder)
         return ''.join(html)
 
 
-    def render_tag_end(self, one_line):
+    def _render_tag_end(self, one_line):
         if one_line:
             return ' />'
         else:
             return '>'
 
 
-    def render_closing_tag(self, indent, tag_name, new_line=False):
+    def _render_closing_tag(self, indent, tag_name, new_line=False):
 
+        res = []
         if new_line:
-            res = ['\n', ' '*indent]
-        else:
-            res = []
+            res += ['\n', ' '*(TAB_INDENT*indent)]
         res += ['</',
                 tag_name,
                 '>',
-               ]
+              ]
+
         return ''.join(res)
+
+    def __repr__(self):
+
+        return "<%s>" % self.tag_name
+class Template(object):
+
+    class ParseError(Exception):
+        pass
+
+    TAB_INDENT = 4
+
+
+    def is_comment(self, line):
+        return line.strip().startswith('//')
+
+
+    def is_tag(self, line):
+        return line.strip().startswith('%')
+
+
+    def is_value_insert(self, line):
+        return line.startswith('=')
+
+
+    def handle_div(self, line):
+        if line.startswith(".") or line.startswith("#"):
+            return "%div" + line
+        else:
+            return line
+
+    def indentation_depth(self, line):
+        indent = 0
+        for char in line:
+            if char == ' ':
+                indent += 1
+            elif char == '\t':
+                indent += Template.TAB_INDENT
+            else:
+                break
+        return line.lstrip(), indent
 
 
     def render(self, context, markup):
-        tag_stack = []
+        tag_stack = [(Node(context, ''), -1)]
         old_indent = None
         old_tag_name = None
         last_line_had_tag = False
         last_line_had_remainder = False
+
+
         for line in markup.split("\n"):
+
+
             if not line:
                 continue
             if self.is_comment(line):
                 continue
 
             stripped_line, indent = self.indentation_depth(line)
-
-            if (last_line_had_tag
-                and old_tag_name is not None
-                and not last_line_had_remainder):
-                    yield self.render_tag_end(one_line=False)
-
-
-            if indent > old_indent:
-                if last_line_had_tag:
-                    tag_stack.append((old_tag_name, old_indent, False))
-
-            else:
-                if last_line_had_tag and last_line_had_remainder:
-                    yield self.render_closing_tag(old_indent, old_tag_name, False)
-                    last_line_had_remainder = False
-                for closing_tag, closing_indent, new_line in reversed(tag_stack):
-                    if closing_indent >= indent:
-                        yield self.render_closing_tag(closing_indent, closing_tag, new_line)
-                        tag_stack.pop()
-                        last_line_had_remainder = False
-
             stripped_line = self.handle_div(stripped_line)
 
+            for closed_node, closed_indent in reversed(tag_stack):
+                if closed_indent >= indent:
+                    tag_stack.pop()
+
+            closed_node, closed_indent = tag_stack[-1]
+
             if self.is_tag(stripped_line):
-                if tag_stack:
-                    closing_tag, closing_indent, new_line = tag_stack.pop()
-                    tag_stack.append((closing_tag, closing_indent, True))
-                tag_name, attrs, is_value_insert, remainder = self.parse_tag(stripped_line)
-                parsed_attrs = self.parse_attrs(attrs)
-                tag_name, parsed_attrs = self.handle_shortcuts(tag_name, parsed_attrs)
-
-                yield self.render_tag_start(context,
-                                            indent,
-                                            tag_name,
-                                            parsed_attrs,
-                                            remainder,
-                                            eval_remainder=is_value_insert)
-                old_tag_name = tag_name
-                last_line_had_tag = True
-                if remainder:
-                    last_line_had_remainder = True
-                else:
-                    last_line_had_remainder = False
-                old_indent = indent
+                node = TagNode(context, stripped_line)
             else:
-                if last_line_had_remainder:
-                    yield ' '
-                    yield line.lstrip()
-                elif last_line_had_tag:
-                    yield '\n'
-                    yield line
-                else:
-                    yield ' '
-                    yield line.lstrip()
-                last_line_had_tag = False
-                last_line_had_remainder = False
+                node = TextNode(context, stripped_line)
 
-        if last_line_had_tag:
-            if last_line_had_remainder:
-                yield self.render_closing_tag(old_indent, old_tag_name)
-            else:
-                yield self.render_tag_end(one_line=True)
-        for closing_tag, closing_indent, new_line in reversed(tag_stack):
-            yield self.render_closing_tag(closing_indent, closing_tag, new_line)
+            closed_node.add_child(node)
+
+            if node.is_tag:
+                tag_stack.append((node, indent))
+
+        root_node, root_indent = tag_stack[0]
+        yield root_node.render(0)
+
--- pyttp/tests/test_template.py
+++ pyttp/tests/test_template.py
@@ -3,7 +3,7 @@ import unittest
 import sys
 sys.path.append("../..")
 
-from pyttp.template import Template
+from pyttp.template import Node, TagNode, TextNode, Template
 
 
 class RenderTests(unittest.TestCase):
@@ -45,14 +45,16 @@ class RenderTests(unittest.TestCase):
     def test_parse_tag(self):
         line = "%a(href: 'bar', target: '_blank')= foo"
 
-        self.assertEqual(self.template.parse_tag(line), ('a',
+        tag_node = TagNode({}, line)
+
+        self.assertEqual(tag_node._parse_tag(), ('a',
                                                     "href: 'bar', target: '_blank'",
                                                     True,
                                                     "foo"))
     def test_parse_attrs(self):
         attrs = "href: 'bar', target: \"foo\""
-
-        self.assertEqual(self.template.parse_attrs(attrs), [('href', 'bar'), ('target', 'foo')])
+        tag_node = TagNode({}, "%dummy")
+        self.assertEqual(tag_node._parse_attrs(attrs), [('href', 'bar'), ('target', 'foo')])
 
 
     def test_handle_div(self):
@@ -65,10 +67,12 @@ class RenderTests(unittest.TestCase):
 
 
     def test_handle_shortcuts(self):
+        tag_node = TagNode({}, "%dummy")
+
         tag_string = "div#title.big.boxed"
         attrs = [('style', 'float: left;')]
 
-        name, attrs = self.template.handle_shortcuts(tag_string, attrs)
+        name, attrs = tag_node._handle_shortcuts(tag_string, attrs)
 
         self.assertEqual(name, "div")
         self.assertEqual(attrs, [('style', 'float: left;'),
@@ -76,6 +80,7 @@ class RenderTests(unittest.TestCase):
                                  ('id', 'title')])
 
     def test_invalid_attrs(self):
+        tag_node = TagNode({}, "%dummy")
         missing_key = "'bar', target: 'foo'"
         missing_value = "href: 'bar', target: "
         wrong_delimiter = "href: 'bar'; target: 'foo'"
@@ -83,29 +88,33 @@ class RenderTests(unittest.TestCase):
         mixed_quotes = "href: 'bar\"; target: 'foo'"
         doubled_quotes = "href: ''bar''; target: 'foo'"
 
-        self.assertRaises(Template.ParseError, self.template.parse_attrs, missing_key)
-        self.assertRaises(Template.ParseError, self.template.parse_attrs, missing_value)
-        self.assertRaises(Template.ParseError, self.template.parse_attrs, wrong_delimiter)
-        self.assertRaises(Template.ParseError, self.template.parse_attrs, missing_quotes)
-        self.assertRaises(Template.ParseError, self.template.parse_attrs, mixed_quotes)
-        self.assertRaises(Template.ParseError, self.template.parse_attrs, doubled_quotes)
+        self.assertRaises(Node.ParseError, tag_node._parse_attrs, missing_key)
+        self.assertRaises(Node.ParseError, tag_node._parse_attrs, missing_value)
+        self.assertRaises(Node.ParseError, tag_node._parse_attrs, wrong_delimiter)
+        self.assertRaises(Node.ParseError, tag_node._parse_attrs, missing_quotes)
+        self.assertRaises(Node.ParseError, tag_node._parse_attrs, mixed_quotes)
+        self.assertRaises(Node.ParseError, tag_node._parse_attrs, doubled_quotes)
 
 
     def test_render_tag_start(self):
+
+        tag_node = TagNode({}, "%li")
+        tag_node.add_child(TextNode({}, "I'm a list item"))
 
         indent = 4
         tag_name = 'li'
         remainder = "I'm a list item"
         attrs = ''
 
-        rendered = self.template.render_tag_start({}, indent, tag_name, attrs, remainder)
-        self.assertEqual(rendered, "\n    <li>I'm a list item")
+        rendered = tag_node._render_tag_start({}, indent, tag_name, attrs, remainder)
+        self.assertEqual(rendered, "\n                <li>I'm a list item")
 
 
     def test_render_tag_end(self):
+        tag_node = TagNode({}, "%dummy")
 
-        self.assertEqual(self.template.render_tag_end(one_line=True), " />")
-        self.assertEqual(self.template.render_tag_end(one_line=False), ">")
+        self.assertEqual(tag_node._render_tag_end(one_line=True), " />")
+        self.assertEqual(tag_node._render_tag_end(one_line=False), ">")
 
 
     def test_render(self):
@@ -120,6 +129,7 @@ class RenderTests(unittest.TestCase):
 %html
     %body
         #title.big.boxed= title
+            %img(src: '= link')
             %p= title
                 bla
                 foo
@@ -127,16 +137,7 @@ class RenderTests(unittest.TestCase):
         %a.bold#link(href: '= link', target: "_blank")= greeting['obj'].en
         blub
 """
-        expected = """
-<html>
-    <body>
-        <div class="big boxed" id="title">Guten Tag
-            <p>Guten Tag bla foo</p>
-        </div>
-        <a href="http://example.com" target="_blank" class="bold" id="link">hello</a>
-        blub
-    </body>
-</html>"""
+        expected = '\n<html>\n    <body>\n        <div class="big boxed" id="title">Guten Tag \n            <img src="http://example.com" /> \n            <p>Guten Tag bla foo</p>\n        </div> \n        <a href="http://example.com" target="_blank" class="bold" id="link" />hello blub\n    </body>\n</html>'
 
         context = dict(title="Guten Tag",
                        link="http://example.com",
@@ -147,4 +148,32 @@ class RenderTests(unittest.TestCase):
         print markup
         print '=' * 80
         print rendered
+        print '=' * 80
+        print expected
         self.assertEqual(rendered, expected)
+
+
+class NodeTests(unittest.TestCase):
+
+    def test_render(self):
+
+        ctx = {'link': 'example.com',
+               'label': 'click me'}
+
+        root_node = TagNode(ctx, "%html")
+        body_node = TagNode(ctx, "%body")
+        div_node = TagNode(ctx, "%div.big this is a test")
+        a_node = TagNode(ctx, "%a(href: '= link')= label")
+        text_node = TextNode(ctx, "hello")
+
+        root_node.add_child(body_node)
+        body_node.add_child(div_node)
+        body_node.add_child(a_node)
+        div_node.add_child(text_node)
+
+        print root_node.render(0)
+
+
+if __name__ == "__main__":
+
+    unittest.main()        
