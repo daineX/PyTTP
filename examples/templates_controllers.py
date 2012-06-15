f7c93e1bda5c444545095d5e256b0437f4e28f4f
daineX <puseidel@gmail.com>
2012-06-15 18:42:12 +0200

    Added shop example page

--- examples/templates_controllers.py
+++ examples/templates_controllers.py
@@ -17,6 +17,12 @@ from fileserve import FileServe
 
 pyttp_config.global_config.setValue("TEMPLATE_SEARCH_PATH", os.path.join(os.path.dirname(__file__), "templates"))
 
+class ShopItem(object):
+
+    def __init__(self, title, price):
+        self.title = title
+        self.price = price
+
 class RootController(Controller):
 
 
@@ -26,6 +32,15 @@ class RootController(Controller):
         date = datetime.now().isoformat()
         return TemplateResponse("index.pyml", context=dict(date=date))
 
+
+    @expose
+    def shop(self, request, **kwargs):
+        items = [ShopItem("SuperAwsome", "100"),
+                 ShopItem("Best product ever", "200"),
+                 ShopItem("I cannot believe it's not Live!", "100"),]
+
+        return TemplateResponse("shop.pyml", context=dict(items=items))
+        
 
 
 if __name__ == "__main__":
