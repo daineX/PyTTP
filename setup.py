511f001860fefa04125ffacc7db1e0357df5a2e2
Paul Seidel <puseidel@gmail.com>
2012-09-11 01:47:47 +0200

    Up version number

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 setup(
     name = "PyTTP",
-    version = "0.1.0",
+    version = "0.1.1",
     author = "Paul Seidel",
     author_email = "puseidel@gmail.com",
     description = ("Small and dirty WSGI-compliant server written entirely in Python"),
