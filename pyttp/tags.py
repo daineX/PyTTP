2d8a25501a0282cc4c27c061c5cdbdec23da2852
Paul Seidel <puseidel@gmail.com>
2012-10-07 15:36:11 +0200

    Add tagging

---
+++ pyttp/tags.py
@@ -0,0 +1,112 @@
+
+from pyttp.database import DataBaseObj, hasField
+
+
+class TagRegistry(object):
+
+    REGISTRY = None
+
+
+    def __init__(self):
+        self.cls_lookup = dict()
+        self.spec_lookup = dict()
+
+
+    @classmethod
+    def register(cls, cls_obj, cls_spec):
+        cls._init_registry()
+        cls.REGISTRY.cls_lookup[cls_spec] = cls_obj
+        cls.REGISTRY.spec_lookup[cls_obj] = cls_spec
+
+
+    @classmethod
+    def _init_registry(cls):
+        if cls.REGISTRY is None:
+            cls.REGISTRY = cls()
+
+
+    @classmethod
+    def get_cls(cls, cls_spec):
+        cls._init_registry()
+        return cls.REGISTRY.cls_lookup.get(cls_spec)
+
+
+    @classmethod
+    def get_spec(cls, cls_obj):
+        cls._init_registry()
+        return cls.REGISTRY.spec_lookup.get(cls_obj)
+
+
+def is_tagged(cls_spec):
+    def decorator(cls):
+        TagRegistry.register(cls, cls_spec)
+        return cls
+    return decorator
+
+
+
+class Tag(DataBaseObj):
+    hasField(unicode, "name")
+    hasField(int, "obj_id")
+    hasField(unicode, "cls_spec")
+
+    @classmethod
+    def get_objs_by_name(cls, name, cls_spec=None):
+        if cls_spec:
+            obj_ids = [x.obj_id for x in cls.select_cond("name = ? and cls_spec = ?", (name, cls_spec))]
+            cls_obj = TagRegistry.get_cls(cls_spec)
+            assert cls_obj
+            for obj_id in obj_ids:
+                yield cls_obj.select_id(obj_id)
+        else:
+            tag_objs = cls.select_cond("name = ?", (name,))
+            for tag in tag_objs:
+                cls_obj = TagRegistry.get_cls(tag.cls_spec)
+                assert cls_obj
+                yield cls_obj.select_id(tag.obj_id)
+
+
+def set_tag(obj, name):
+    assert obj.id
+    cls_spec = TagRegistry.get_spec(obj.__class__)
+    assert cls_spec
+    tag = Tag(name=name, obj_id=obj.id, cls_spec=cls_spec)
+    tag.save()
+
+
+def remove_tag(obj, name):
+    cls_spec = TagRegistry.get_spec(obj.__class__)
+    assert cls_spec
+    try:
+        tag = Tag.select_cond("name = ? and cls_spec = ? and obj_id = ?", (name, cls_spec, obj.id)).next()
+        Tag.delete(tag)
+    except StopIteration:
+        pass
+
+
+def set_tags(obj, tag_line):
+    assert obj.id
+    cls_spec = TagRegistry.get_spec(obj.__class__)
+    assert cls_spec
+    current_tags = set(tag.name for tag in
+                      Tag.select_cond("cls_spec = ? and obj_id = ?", (cls_spec, obj.id)))
+    tags_to_set = set([x.strip() for x in tag_line.split(",")])
+    tags_to_delete = current_tags - tags_to_set
+    tags_to_set = tags_to_set - current_tags
+
+    for tag_name in tags_to_delete:
+        remove_tag(obj, tag_name)
+    for tag_name in tags_to_set:
+        set_tag(obj, tag_name)
+
+
+def get_tags(obj):
+    cls_spec = TagRegistry.get_spec(obj.__class__)
+    assert cls_spec
+    current_tags = set(Tag.select_cond("cls_spec = ? and obj_id = ?", (cls_spec, obj.id)))
+    return current_tags
+
+
+def get_tag_line(obj):
+    return ', '.join(tag.name for tag in get_tags(obj))
+
---
+++ pyttp/tests/test_tags.py
@@ -0,0 +1,90 @@
+import unittest
+
+import sys
+sys.path.append("../..")
+
+from pyttp import database as db
+import sqlite3
+
+from pyttp.tags import *
+
+
+@is_tagged("tagged_obj")
+class TaggedObject(db.DataBaseObj):
+    hasField(unicode, "name")
+    hasField(int, "number")
+
+
+@is_tagged("another_tagged_obj")
+class AnotherTaggedObject(db.DataBaseObj):
+    hasField(unicode, "spam")
+    hasField(int, "eggs")
+
+
+class TagTests(unittest.TestCase):
+
+    def setUp(self):
+        conn = sqlite3.connect(":memory:")
+        db.globalConnObj = conn
+        conn.row_factory = sqlite3.Row
+        TaggedObject.create()
+        AnotherTaggedObject.create()
+        Tag.create()
+
+
+    def test_set_tag(self):
+        obj1 = TaggedObject(name="foo", number=1)
+        obj1.save()
+        obj2 = AnotherTaggedObject(spam="bla", eggs=4)
+        obj2.save()
+        set_tag(obj1, "bar")
+        set_tag(obj2, "bar")
+
+        tagged_objs = list(Tag.get_objs_by_name("bar"))
+        self.assertTrue(obj1 in tagged_objs)
+        self.assertTrue(obj2 in tagged_objs)
+
+
+    def test_filter_objs_by_cls(self):
+        obj1 = TaggedObject(name="foo", number=1)
+        obj1.save()
+        obj2 = AnotherTaggedObject(spam="bla", eggs=4)
+        obj2.save()
+        set_tag(obj1, "bar")
+        set_tag(obj2, "bar")
+
+        tagged_objs = list(Tag.get_objs_by_name("bar", cls_spec="tagged_obj"))
+        self.assertTrue(obj1 in tagged_objs)
+        self.assertFalse(obj2 in tagged_objs)
+
+
+    def test_remove_tag(self):
+        obj1 = TaggedObject(name="foo", number=1)
+        obj1.save()
+        set_tag(obj1, "bar")
+
+        tagged_objs = list(Tag.get_objs_by_name("bar", cls_spec="tagged_obj"))
+        self.assertTrue(obj1 in tagged_objs)
+
+        remove_tag(obj1, "bar")
+
+        tagged_objs = list(Tag.get_objs_by_name("bar", cls_spec="tagged_obj"))
+        self.assertFalse(obj1 in tagged_objs)
+
+
+    def test_set_tags(self):
+        obj1 = TaggedObject(name="foo", number=1)
+        obj1.save()
+
+        set_tags(obj1, "test, debug, PyTTP")
+        current_tags = get_tag_line(obj1)
+        self.assertTrue("test" in current_tags)
+        self.assertTrue("debug" in current_tags)
+        self.assertTrue("PyTTP" in current_tags)
+
+        set_tags(obj1, "test, html")
+        current_tags = get_tag_line(obj1)
+        self.assertTrue("test" in current_tags)
+        self.assertTrue("html" in current_tags)
+        self.assertFalse("debug" in current_tags)
+        self.assertFalse("PyTTP" in current_tags)
