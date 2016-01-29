e48f13bd43af1f9dcb61189910d21a2cecc988e7
Paul Seidel <pse@ableton.com>
2016-01-29 19:16:35 +0100

    Revert regular expression replacement

--- pyttp/html.py
+++ pyttp/html.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 
-import re
 import types
 
 html_escape_table = {
@@ -12,15 +11,10 @@ html_escape_table = {
     u"<": u"&lt;",
     }
 
-REX = r'|'.join(html_escape_table.iterkeys())
-def get_replacement(match):
-    match = match.string
-    return html_escape_table.get(match, match)
-
 def html_escape(text):
     """Produce entities within text."""
     if isinstance(text, str) or isinstance(text, unicode):
-        return re.sub(REX, get_replacement, text)
+        return u"".join(html_escape_table.get(c,c) for c in text)
     else:
         return str(text)
 
