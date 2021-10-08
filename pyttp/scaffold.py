58f385acf964aadcfdc78a7b9c3ed9d3ebaf685c
Paul Seidel <paul.seidel@ableton.com>
2021-10-08 10:26:30 +0200

    Add convenience function to create contoller-based app to be used with WSGI implementations

--- pyttp/scaffold.py
+++ pyttp/scaffold.py
@@ -7,6 +7,23 @@ from .apps import FileServer, Router
 from .controller import ControllerWSGIApp
 from .wsgi import WSGIListener
 
+def make_controller_root(
+        controller,
+        static_serve_url='/static/.+',
+        static_serve_split=1,
+        static_serve_dir=None):
+    if static_serve_dir is None:
+        static_serve_dir = os.path.join(settings.BASE_DIR, "static")
+    fileserve_app = FileServer(static_serve_dir)
+    controller_app = ControllerWSGIApp(controller)
+    root_app = Router([(static_serve_url, fileserve_app, static_serve_split),
+                       ('/.*', controller_app)])
+    return root_app
+
+def wrap_root(root_app, port=8080, nThreads=1, **kwargs):
+    print(nThreads)
+    return WSGIListener(root_app, port, nThreads=nThreads, **kwargs)
+
 def make_controller_listener(controller,
                              port=8080,
                              static_serve_url='/static/.+',
@@ -14,11 +31,10 @@ def make_controller_listener(controller,
                              static_serve_dir=None,
                              **kwargs
                              ):
-    if static_serve_dir is None:
-        static_serve_dir = os.path.join(settings.BASE_DIR, "static")
-    fileserve_app = FileServer(static_serve_dir)
-    controller_app = ControllerWSGIApp(controller)
-    root_app = Router([(static_serve_url, fileserve_app, static_serve_split),
-                       ('/.*', controller_app)])
-
-    return WSGIListener(root_app, port, **kwargs)
+    root_app = make_controller_root(
+        controller,
+        static_serve_url=static_serve_url,
+        static_serve_split=static_serve_split,
+        static_serve_dir=static_serve_dir,
+    )
+    return wrap_root(root_app, port, **kwargs)
